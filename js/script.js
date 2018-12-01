b4361330d9f47515caff6ed114c520c90cd8bfab
Marcin <dudzicmarcinprivate@gmail.com>
2018-12-01 21:36:01 +0100

    Remove js line

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ window.onload = changeHeaderColor;
 function changeHeaderText(){
     let texts = ["twoich pomysłów.", "twojego biznesu.", "twojego sukcesu.", "twojej pasji.", "ciebie."]
     const changedText = document.querySelector('.frontpage-header-title-changed');
-    // changedText.innerHTML = texts[Math.floor(Math.random() * (texts.length - 1))];
     let i = 0;
     setInterval(() => {
         if(i <= 4){
